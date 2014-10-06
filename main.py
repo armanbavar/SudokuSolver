28d0332d05b865df93e147baf50bee2da751b94e
Emil Olsson <emilolsson93@gmail.com>
2014-10-06 20:53:53 +0200

    You can do some cool stuff with python
    * Need to learn python :facepunch:

---
+++ main.py
@@ -0,0 +1,17 @@
+# Ints
+print (1 + 1)
+
+# string
+print ("Print a string")
+
+# String + int WOW!!!
+print ("Print this string with this int: " + str(1)) 
+
+# This you can't do in C#!!! WOW!!!
+print ([1, "I'm in the middle of this array?!?!?", 0])
+
+# Python is so awesome!!!
+array = [1, 2, 3, 4, 5] 
+print ([(str(c) + "python") for c in array])
+
+print ([i for i in array if i != 5])
